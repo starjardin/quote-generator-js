eab0347534cf2084bf0359f77b950d0fc70e1eb7
Tantely Andrianarivola <tantely.and@onja.org>
2020-06-05 09:59:31 +0300

    quote to the new quotation

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -22,32 +22,38 @@ let randomElement3 = lastSentence[randomNumber3];
 
 //This is the full quoteation
 
-let fullQuote = (randomElement + randomElement2 + randomElement3)
-// console.log(`'${fullQuote}'`);
+let fullQuote = (randomElement + ' ' + randomElement2 + ' ' +  randomElement3)
+console.log(`'${fullQuote}'`);
 // Chose the number of quotes generated, we use peompt for that.
 let number = prompt('Enter a number between 1 to 5 here');
-
+//Check if number is equal to one quotes equal to firstQoute
 if (number === "1") {
   let firsrQuote = (firstSentece[0] + secondSentence[0] + lastSentence[0]);
-  console.log(firsrQuote)
+  console.log(`'${firsrQuote}'`);
 }
 
+//Check if number is equal to two quotes equal to secondtQoute
 if (number === "2") {
   let secondQuote = (firstSentece[1] +', ' + secondSentence[1] + ', ' + lastSentence[1]);
-  console.log(secondQuote)
+  console.log(`'${secondQuote}'`)
 }
+//Check if number is equal to three quotes equal to thirddtQoute
 
 if (number === "3") {
   let thirdQuote = (firstSentece[2] +', ' + secondSentence[2] + ', ' + lastSentence[2]);
-  console.log(thirdQuote)
+  console.log(`'${thirdQuote}'`)
 }
+
+//Check if number is equal to four quotes equal to fourthQoute
 
 if (number === "4") {
   let secondQuote = (firstSentece[3] +', ' + secondSentence[3] + ', ' + lastSentence[3]);
-  console.log(secondQuote)
+  console.log(`'${secondQuote}'`)
 }
 
+//Check if number is equal to five quotes equal to fifhtQoute
+
 if (number === "5") {
-  let secondQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
-  console.log(secondQuote)
+  let fithQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+  console.log(`'${fithQuote}'`)
 }
