61b152aec5e2fc3e02e5808bceb5539a0edd4d50
Tantely Andrianarivola <tantely.and@onja.org>
2020-06-05 10:51:01 +0300

    improving the fisrt code using switch methode

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -26,34 +26,68 @@ let fullQuote = (randomElement + ' ' + randomElement2 + ' ' +  randomElement3)
 console.log(`'${fullQuote}'`);
 // Chose the number of quotes generated, we use peompt for that.
 let number = prompt('Enter a number between 1 to 5 here');
-//Check if number is equal to one quotes equal to firstQoute
-if (number === "1") {
-  let firsrQuote = (firstSentece[0] + ' ' + secondSentence[0] + ' ' + lastSentence[0]);
-  console.log(`'${firsrQuote}'`);
-}
 
-//Check if number is equal to two quotes equal to secondtQoute
-if (number === "2") {
-  let secondQuote = (firstSentece[1] +', ' + secondSentence[1] + ', ' + lastSentence[1]);
-  console.log(`'${secondQuote}'`)
-}
-//Check if number is equal to three quotes equal to thirddtQoute
+// //Check if number is equal to one quotes equal to firstQoute
+// if (number === "1") {
+//   let firstQuote = (firstSentece[0] + ' ' + secondSentence[0] + ' ' + lastSentence[0]);
+//   console.log(`'${firstQuote}'`);
+// }
 
-if (number === "3") {
-  let thirdQuote = (firstSentece[2] +', ' + secondSentence[2] + ', ' + lastSentence[2]);
-  console.log(`'${thirdQuote}'`)
-}
+// //Check if number is equal to two quotes equal to secondtQoute
+// if (number === "2") {
+//   let secondQuote = (firstSentece[1] +', ' + secondSentence[1] + ', ' + lastSentence[1]);
+//   console.log(`'${secondQuote}'`)
+// }
+// //Check if number is equal to three quotes equal to thirddtQoute
 
-//Check if number is equal to four quotes equal to fourthQoute
+// if (number === "3") {
+//   let thirdQuote = (firstSentece[2] +', ' + secondSentence[2] + ', ' + lastSentence[2]);
+//   console.log(`'${thirdQuote}'`)
+// }
 
-if (number === "4") {
-  let secondQuote = (firstSentece[3] +', ' + secondSentence[3] + ', ' + lastSentence[3]);
-  console.log(`'${secondQuote}'`)
-}
+// //Check if number is equal to four quotes equal to fourthQoute
 
-//Check if number is equal to five quotes equal to fifhtQoute
+// if (number === "4") {
+//   let secondQuote = (firstSentece[3] +', ' + secondSentence[3] + ', ' + lastSentence[3]);
+//   console.log(`'${secondQuote}'`)
+// }
 
-if (number === "5") {
-  let fithQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
-  console.log(`'${fithQuote}'`)
+// //Check if number is equal to five quotes equal to fifhtQoute
+
+// if (number === "5") {
+//   let fithQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+//   console.log(`'${fithQuote}'`)
+// }
+
+//Using switch methode
+
+switch (number) {
+  case "1":
+    //Check the number is equal to one.
+    let firstQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+    console.log(firstQuote);
+    break;
+    //Check if the number is equal to two.
+  case "2":
+    let secondQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+    console.log(secondQuote);
+    break;
+    //Check if the number is equal to three.
+  case "3":
+    let thirdQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+    console.log(thirdQuote);
+    break;
+    //Check if the number is equal to four.
+  case "4":
+    let fourthQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+    console.log(fourthQuote);
+    break;
+    //Check if the number is equal to five.
+  case "5":
+    let fithQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+    console.log(fithQuote);
+    break;
+    //If the number is not between 0 and 6 This will throw an alert message.
+  default:
+    console.log(`'Wow not found please try again with a number less than 6'`);
 }
