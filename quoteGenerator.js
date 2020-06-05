f94cc1b46416205af64973137a60d49a834379c2
Tantely Andrianarivola <tantely.and@onja.org>
2020-06-05 09:49:22 +0300

    you can chose a specific quotation epends on what number you display in the prompt

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,14 +1,14 @@
 //Create an array for the first pieces of sentence
 let firstSentece = [
-  'Hello world', 'hi there', 'Good morning'
+  'Hello world', 'hi there', 'Good morning', 'I am Will', 'I am Malgasy'
 ]
 //Create an array for the second pieces of sentence
 let secondSentence = [
-  'I am good', 'And you', 'I am doing ok'
+  'I am good', 'And you', 'I am doing ok', 'Nice to meet you', 'Good to see you again'
 ]
 // Create an array for the third peices of sentence
 let lastSentence = [
-  'Wow!!', 'Good bye', 'See you'
+  'Wow!!', 'Good bye', 'See you', 'catch you later', 'Good luck'
 ]
 // Call the peices of sentences radomly
 let randomNumber1 = Math.floor(Math.random() * firstSentece.length);
@@ -23,18 +23,31 @@ let randomElement3 = lastSentence[randomNumber3];
 //This is the full quoteation
 
 let fullQuote = (randomElement + randomElement2 + randomElement3)
-console.log(`'${fullQuote}'`);
+// console.log(`'${fullQuote}'`);
 // Chose the number of quotes generated, we use peompt for that.
 let number = prompt('Enter a number between 1 to 5 here');
 
-// for (let i = 0; i < 5; i++) {
-//   switch(randomNumber1) {
-//     case "1":
-//       let value1 = firstSentece[0]
-//       alert(value1);
-//       break;
-//     case "2":
-//       let value2 = firstSentece[1];
-//       alert(value2)  
-//   }
-// }
+if (number === "1") {
+  let firsrQuote = (firstSentece[0] + secondSentence[0] + lastSentence[0]);
+  console.log(firsrQuote)
+}
+
+if (number === "2") {
+  let secondQuote = (firstSentece[1] +', ' + secondSentence[1] + ', ' + lastSentence[1]);
+  console.log(secondQuote)
+}
+
+if (number === "3") {
+  let thirdQuote = (firstSentece[2] +', ' + secondSentence[2] + ', ' + lastSentence[2]);
+  console.log(thirdQuote)
+}
+
+if (number === "4") {
+  let secondQuote = (firstSentece[3] +', ' + secondSentence[3] + ', ' + lastSentence[3]);
+  console.log(secondQuote)
+}
+
+if (number === "5") {
+  let secondQuote = (firstSentece[4] +', ' + secondSentence[4] + ', ' + lastSentence[4]);
+  console.log(secondQuote)
+}
